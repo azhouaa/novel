8025cb536ee4fc5d1ab88fd64c05a210c50d3ee6
azhouaa <1415830597@qq.com>
2026-03-27 14:25:04 +0800

    init

---
+++ novel-front-web-master/src/components/author/Header.vue
@@ -0,0 +1,17 @@
+ <template>
+  <div class="header">
+    <div class="mainNav" id="mainNav">
+        <div class="box_center cf" style="text-align: center;height: 44px;line-height: 48px;color: #fff;font-size: 16px;">
+
+            阿洲小说在线作家管理
+
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "AuthorHeader"
+};
+</script>
