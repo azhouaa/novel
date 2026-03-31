cd900a8cfa05c6216cac78695319b428780cfcaa
azhouaa <1415830597@qq.com>
2026-03-31 22:31:26 +0800

    增加了图片映射功能和小说封面

--- novel-master/src/main/java/com/azhou/novel/core/config/WebConfig.java
+++ novel-master/src/main/java/com/azhou/novel/core/config/WebConfig.java
@@ -9,6 +9,7 @@ import com.azhou.novel.core.interceptor.TokenParseInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -35,33 +36,33 @@ public class WebConfig implements WebMvcConfigurer {
 
         // 流量限制拦截器
         registry.addInterceptor(flowLimitInterceptor)
-            .addPathPatterns("/**")
-            .order(0);
+                .addPathPatterns("/**")
+                .order(0);
 
         // 文件访问拦截
         registry.addInterceptor(fileInterceptor)
-            .addPathPatterns(SystemConfigConsts.IMAGE_UPLOAD_DIRECTORY + "**")
-            .order(1);
+                .addPathPatterns(SystemConfigConsts.IMAGE_UPLOAD_DIRECTORY + "**")
+                .order(1);
 
         // 权限认证拦截
         registry.addInterceptor(authInterceptor)
-            // 拦截会员中心相关请求接口
-            .addPathPatterns(ApiRouterConsts.API_FRONT_USER_URL_PREFIX + "/**",
-                // 拦截作家后台相关请求接口
-                ApiRouterConsts.API_AUTHOR_URL_PREFIX + "/**",
-                // 拦截平台后台相关请求接口
-                ApiRouterConsts.API_ADMIN_URL_PREFIX + "/**")
-            // 放行登录注册相关请求接口
-            .excludePathPatterns(ApiRouterConsts.API_FRONT_USER_URL_PREFIX + "/register",
-                ApiRouterConsts.API_FRONT_USER_URL_PREFIX + "/login",
-                ApiRouterConsts.API_ADMIN_URL_PREFIX + "/login")
-            .order(2);
+                .addPathPatterns(ApiRouterConsts.API_FRONT_USER_URL_PREFIX + "/**",
+                        ApiRouterConsts.API_AUTHOR_URL_PREFIX + "/**",
+                        ApiRouterConsts.API_ADMIN_URL_PREFIX + "/**")
+                .excludePathPatterns(ApiRouterConsts.API_FRONT_USER_URL_PREFIX + "/register",
+                        ApiRouterConsts.API_FRONT_USER_URL_PREFIX + "/login",
+                        ApiRouterConsts.API_ADMIN_URL_PREFIX + "/login")
+                .order(2);
 
         // Token 解析拦截器
         registry.addInterceptor(tokenParseInterceptor)
-            // 拦截小说内容查询接口，需要解析 token 以判断该用户是否有权阅读该章节（付费章节是否已购买）
-            .addPathPatterns(ApiRouterConsts.API_FRONT_BOOK_URL_PREFIX + "/content/*")
-            .order(3);
+                .addPathPatterns(ApiRouterConsts.API_FRONT_BOOK_URL_PREFIX + "/content/*")
+                .order(3);
+    }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/localPic/**")
+                .addResourceLocations("file:F:\\CodeHome\\JavaHome\\JavaProject\\novel\\test\\novel-master\\upload\\localPic\\");
     }
 }
